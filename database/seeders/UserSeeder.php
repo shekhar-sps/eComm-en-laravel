328adb076735392a46890d1edf990facc461e653
shekhar-sps <shekhar1.muppidi@gmail.com>
2021-06-16 18:04:55 +0530

    tables migrations and seeder update

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert([
+            'name'=>'arjun',
+            'email'=>'arjun@gmail.com',
+            'password'=>Hash::make('12345')
+        ]);
+
+    }
+}
